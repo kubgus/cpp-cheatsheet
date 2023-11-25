c2126a027eb5ec72fe4a7c5bd2909ac54f66b483
kubgus <ja.gustafik@outlook.sk>
2023-11-25 02:33:45 +0100

    Lifetime

---
+++ lifetime.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+struct Robot
+{
+    const char *name;
+    Robot(const char *name) : name(name) { std::cout << "Starting up " << this->name << "!" << std::endl; }
+    ~Robot() { std::cout << "Shutting down " << name << "!" << std::endl; }
+};
+
+// Curly braces create a new scope
+// When the scope ends, the object is destroyed
+int main()
+{
+    Robot r2d2("R2-D2");
+
+    {
+        Robot optimus("Optimus");
+    }
+
+    Robot bumblebee("Bumblebee");
+}
+
+// This is important because of a common mistake in C++. Consider this snippet:
+// int *CreateArray()
+// {
+//     int array[50];
+//     return array;
+// }
+// This function returns a pointer to an array of 50 integers.
+// However, the array is destroyed when the function ends.
+// This means that the pointer is now pointing to garbage memory.
+// This is called a dangling pointer.
